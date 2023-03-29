008f89e994ce23468c4077d1ba5988fae823420d
Vish <Vishwareddy@gmail.com>
2023-03-29 16:06:13 -0700

    deleted unnecessary files, added navigation + css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    function handleResize() {
+      setWindowWidth(window.innerWidth);
+    }
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+    return () => window.removeEventListener('resize', handleResize);
+  }, [window.innerWidth]);
+
+  function handleNavClick(){
+    console.log('clicked')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <div className='title'>
+          <h1 className='titletext'>THE CHEMISTRY CALCULATOR</h1>
+        </div>
+
+        <div style={{width: windowWidth*.2}}>
+          <div className='nav'>
+              <a onClick={handleNavClick}>Chemistry Reactions </a>
+              <a onClick={handleNavClick}>Gas Laws</a>
+          </div>
+        </div>
+    </>
   );
 }
 
