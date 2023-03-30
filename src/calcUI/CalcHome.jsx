2002b04e040766c8b2384a8fe1bbc572f00f5718
Vish <Vishwareddy@gmail.com>
2023-03-30 00:07:48 -0700

    finished logic and UI of general calc

--- src/calcUI/CalcHome.jsx
+++ src/calcUI/CalcHome.jsx
@@ -2,13 +2,19 @@ import '../css/CalcHome.css';
 import { useState, useEffect } from 'react';
 
 function CalcHome() {
- 
+    const [submittedText, setSubmittedText] = useState("")
+    function handleSubmit (){
+        setSubmittedText("clicked submit");
+    }
+
   return (
-    <>
-    <h1>GENERAL CALC UI</h1>
-    <input placeholder='ENTER SOME CHEMISTRY' />
-    <button>Calculate</button>
-    </>
+    <div className='calc-container'>
+        <h1>GENERAL CALC UI</h1>
+        <input placeholder='ENTER SOME CHEMISTRY' onChange={(val)=>{setSubmittedText(val.target.value);}} />
+        <button className='submit' onClick={handleSubmit}>Calculate</button>
+        <h3>Result:</h3>
+        <h4>{submittedText}</h4>
+    </div>
   );
 }
 
